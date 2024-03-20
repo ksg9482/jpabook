a91697d85cfe6d0077172bb7098326d57959c17a
ksg9482 <ksg930523@gmail.com>
2024-03-20 21:18:25 +0900

    jpashop- order service

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,13 +2,16 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //protected 생성자와 동일.
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -27,16 +30,18 @@ public class OrderItem {
 
     private int count; //주문 당시 수량.
 
+    //JPA는 protected까지 기본생성자를 지원함. protected를 통해 new를 통한 생성을 방지
+//    protected OrderItem() {
+//
+//    }
     //생성이 단순하지 않음
-
-
     //==생성 메서드==//
     public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
         OrderItem orderItem = new OrderItem();
         orderItem.setItem(item);
         orderItem.setOrderPrice(orderPrice);
 
-        //가격 -> 쿠폰 받거나 할인 받거나 등 변수가 많음.
+        //가격 -> item에 가격이 있지만 쿠폰 받거나 할인 받거나 등 변수가 많음. 그래서 처리 끝난 결과물을 주입하는 것.
         orderItem.setCount(count);
 
         //재고도 줄어들어야 한다
